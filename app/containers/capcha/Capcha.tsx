0ad66d541b921e9d40318a73c7cfdd38decbcad7
Eloik-dev <eloik.rousseau@gmail.com>
2025-11-01 18:39:37 -0400

    Update

--- app/containers/capcha/Capcha.tsx
+++ app/containers/capcha/Capcha.tsx
@@ -7,7 +7,7 @@ import { MinigamesProvider } from "~/context/MinigamesContext";
 
 const Capcha = () => {
     return (
-        <MinigamesProvider levels={[<Clicker />, <Jeopardy />]} />
+        <MinigamesProvider levels={[<Tictacto />, <Comet />, <Snake />, <Clicker />, <Jeopardy />]} />
     );
 }
 
