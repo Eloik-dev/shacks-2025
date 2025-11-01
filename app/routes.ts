41301e03f5537b43454daf21b97c0224a6d8d243
MathisCantin <canm3144@usherbrooke.ca>
2025-11-01 13:10:20 -0400

    Landing page

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
 	index("routes/home/home.tsx"),
 	route("capchat", "routes/capchat/capchat.tsx"),
+	// Catch-all 404 route (should be last)
+	route("*", "routes/404/notfound.tsx"),
 ] satisfies RouteConfig;
